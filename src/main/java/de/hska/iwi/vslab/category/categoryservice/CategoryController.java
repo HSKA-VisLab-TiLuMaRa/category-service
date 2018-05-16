96ebb2a26c50b14da77691f5ad08a48dad1ea275
Raphael Hippe <info@raphaelhippe.de>
2018-05-16 14:08:32 +0200

    added query usage for categories get call

--- src/main/java/de/hska/iwi/vslab/category/categoryservice/CategoryController.java
+++ src/main/java/de/hska/iwi/vslab/category/categoryservice/CategoryController.java
@@ -1,6 +1,7 @@
 package de.hska.iwi.vslab.category.categoryservice;
 
 import java.net.URI;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -20,8 +22,14 @@ public class CategoryController {
 	private CategoryRepo repo;
 
 	@RequestMapping(value = "/categories", method = RequestMethod.GET)
-	public ResponseEntity<Iterable<Category>> getCategorys() {
-		Iterable<Category> allPolls = repo.findAll();
+	public ResponseEntity<Iterable<Category>> getCategorys(@RequestParam Optional<String> name) {
+		// Iterable<Category> allPolls = repo.findAll();
+		Iterable<Category> allPolls;
+		if (name.isPresent()){
+			allPolls = repo.findCategoriesByName(name.get());
+		} else {
+			allPolls = repo.findAll();
+		}
 		return new ResponseEntity<Iterable<Category>>(allPolls, HttpStatus.OK);
 	}
 
@@ -52,7 +60,7 @@ public class CategoryController {
 		responseHeaders.setLocation(newCategoryUri);
 		return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
 	}
-	
+
 	@RequestMapping(value = "/categories/{categoryId}", method = RequestMethod.DELETE)
 	public ResponseEntity<Category> deleteUser(@PathVariable Long categoryId) {
 		repo.deleteById(categoryId);
--- src/main/java/de/hska/iwi/vslab/category/categoryservice/CategoryRepo.java
+++ src/main/java/de/hska/iwi/vslab/category/categoryservice/CategoryRepo.java
@@ -1,3 +1,12 @@
 package de.hska.iwi.vslab.category.categoryservice;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-public interface CategoryRepo extends CrudRepository<Category, Long> { }
+import org.springframework.data.repository.query.Param;
+
+public interface CategoryRepo extends CrudRepository<Category, Long> {
+  @Query("SELECT t FROM Category t where t.name = :name")
+  Iterable<Category> findCategoriesByName(@Param("name") String name);
+
+  // @Query("SELECT t FROM Category t where t.productId = :productId")
+  // Iterable<Category> findCategoriesByProduct(@Param("productId") Integer productId);
+}
